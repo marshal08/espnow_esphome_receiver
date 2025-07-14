b25958c847b5c4c7e070d0c4933adc2c1f2936da
marshal08 <lubomir.beluch@gmail.com>
2025-07-14 12:55:45 +0200

    Update espnow_listener.cpp

--- components/espnow_listener/espnow_listener.cpp
+++ components/espnow_listener/espnow_listener.cpp
@@ -3,12 +3,13 @@
 namespace esphome {
 namespace espnow_listener {
 
-void ESPNowListener::setup() {
-  ESPNowListener::instance = this;  // Save instance for static callback
-  WiFi.mode(WIFI_STA);
-  esp_now_init();
-  esp_now_register_recv_cb(on_espnow_receive);
-}
+ESPNowListener *ESPNowListener::instance = nullptr;
+
+class ESPNowListener : public Component {
+ public:
+  void setup() override;
+  void dump_config() override;
+};
 
 }  // namespace espnow_listener
 }  // namespace esphome
