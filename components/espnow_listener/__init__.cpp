6f4f3d5941b84afaeaa709582bd86e2f3668af98
marshal08 <lubomir.beluch@gmail.com>
2025-07-14 13:08:39 +0200

    Update __init__.cpp

--- components/espnow_listener/__init__.cpp
+++ components/espnow_listener/__init__.cpp
@@ -5,13 +5,9 @@
 namespace esphome {
 namespace espnow_listener {
 
-static const auto COMPONENT_SCHEMA = register_component<ESPNowListener>("espnow_listener")
-  .then([](ESPNowListener *comp, const esphome::yaml::YAML::Node &config) {
-    auto *sensor = new text_sensor::TextSensor();
-    sensor->set_name(config["text_sensor"]["name"].as<std::string>());
-    comp->set_text_sensor(sensor);
-    App.register_text_sensor(sensor);
-  });
+static const auto CONFIG_SCHEMA = register_component<ESPNowListener>("espnow_listener")
+  .overrideable()
+  .add_required_sub_component("text_sensor");
 
 }  // namespace espnow_listener
 }  // namespace esphome
