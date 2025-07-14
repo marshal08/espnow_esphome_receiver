4aff7fd5012dce0466aef60ab333b896aae9b451
marshal08 <lubomir.beluch@gmail.com>
2025-07-14 13:16:46 +0200

    Update espnow_listener.h

--- components/espnow_listener/espnow_listener.h
+++ components/espnow_listener/espnow_listener.h
@@ -1,19 +1,21 @@
 #pragma once
 #include "esphome/core/component.h"
 #include "esphome/components/text_sensor/text_sensor.h"
+#include <esp_now.h>
+#include <WiFi.h>
 
 namespace esphome {
 namespace espnow_listener {
 
 class ESPNowListener : public Component {
  public:
-  void set_text_sensor(text_sensor::TextSensor *sensor) { this->sensor_ = sensor; }
+  void set_text_sensor(text_sensor::TextSensor *sensor);
   void setup() override;
   void dump_config() override;
   void loop() override {}
 
  protected:
-  text_sensor::TextSensor *sensor_;
+  text_sensor::TextSensor *text_sensor_;
 };
 
 }  // namespace espnow_listener
