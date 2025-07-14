9895a86676058de834a90475965fcf357bd58bb9
marshal08 <lubomir.beluch@gmail.com>
2025-07-14 13:17:24 +0200

    Update __init__.cpp

--- components/espnow_listener/__init__.cpp
+++ components/espnow_listener/__init__.cpp
@@ -6,8 +6,12 @@ namespace esphome {
 namespace espnow_listener {
 
 static const auto CONFIG_SCHEMA = register_component<ESPNowListener>("espnow_listener")
-  .overrideable()
-  .add_required_sub_component("text_sensor");
+  .required("text_sensor", [](ESPNowListener *comp, const esphome::yaml::Node &node) {
+    auto *sensor = new esphome::text_sensor::TextSensor();
+    sensor->set_name(node["name"].as<std::string>());
+    comp->set_text_sensor(sensor);
+    esphome::App.register_text_sensor(sensor);
+  });
 
 }  // namespace espnow_listener
 }  // namespace esphome
