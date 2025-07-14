a5986ccc148d52368501306445e8f8d7351ef560
marshal08 <lubomir.beluch@gmail.com>
2025-07-14 13:31:26 +0200

    Update espnow_listener.h

--- components/espnow_listener/espnow_listener.h
+++ components/espnow_listener/espnow_listener.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "esphome/core/component.h"
-#include "esphome/components/text_sensor/text_sensor.h"  // ✅ Correct for ESPHome structure
+#include "esphome/components/text_sensor/text_sensor.h"
 #include <esp_now.h>
 #include <WiFi.h>
 
