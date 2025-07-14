a96635c57a0c5125969480fd027118b4164de9aa
marshal08 <lubomir.beluch@gmail.com>
2025-07-14 13:22:27 +0200

    Update espnow_listener.h

--- components/espnow_listener/espnow_listener.h
+++ components/espnow_listener/espnow_listener.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "esphome/core/component.h"
-#include "esphome/components/text_sensor/text_sensor.h"
+#include "text_sensor/text_sensor.h"
 #include <esp_now.h>
 #include <WiFi.h>
 
